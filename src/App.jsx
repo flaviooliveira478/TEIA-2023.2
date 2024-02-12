9749276f1d4f49120d5785b3fd16be7763cf83ce
Marcos Paulo <marcos.paullo32@gmail.com>
2024-02-12 03:34:37 -0300

    feat: fill using components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,83 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+/* import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg"; */
+import "./index.css";
+import Navbar from "./components/Navbar.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Title from "./components/Title.jsx";
+import Layout from "./components/Layout.jsx";
+import News from "./components/News.jsx";
+import Releases from "./components/Releases.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const destaques = [
+    {
+      imagem: "",
+      tag: "Series",
+      titulo:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptatem quaerat perspiciatis tempor",
+    },
+    {
+      imagem: "",
+      tag: "Tecnologia",
+      titulo:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptatem quaerat perspiciatis tempor",
+    },
+    {
+      imagem: "",
+      tag: "Ciência",
+      titulo:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptatem quaerat perspiciatis tempor",
+    },
+  ];
 
+  const noticias = [
+    {
+      imagem: "imagem1.jpg",
+      tag: "Tag1",
+      title: "Título1",
+      text: "Texto1",
+      lermais: "Ler mais1",
+    },
+    {
+      imagem: "imagem2.jpg",
+      tag: "Tag2",
+      title: "Título2",
+      text: "Texto2",
+      lermais: "Ler mais2",
+    },
+    {
+      imagem: "imagem3.jpg",
+      tag: "Tag3",
+      title: "Título3",
+      text: "Texto3",
+      lermais: "Ler mais3",
+    },
+  ];
+
+  const lancamentos = [
+    {
+      tag: "Tag1",
+      title: "Título1",
+      text: "Texto1",
+      lermais: "Ler mais1",
+    },
+    { tag: "Tag2", title: "Título2", text: "Texto2", lermais: "Ler mais2" },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+      <Title
+        title="Bem-vindo ao Techverse!"
+        paragraph="
+        O Techverse é a sua fonte definitiva de notícias, análises e insights sobre o fascinante universo da tecnologia e muito mais."
+      />
+      <Layout infos={destaques} />
+      <News noticias={noticias} />
+      <Releases dados={lancamentos} />
+      <Footer />
+    </div>
   );
 }
 
