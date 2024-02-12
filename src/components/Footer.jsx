ed2639456142d98a88a3ec3cf5b5ab0bbb16cddb
Marcos Paulo <marcos.paullo32@gmail.com>
2024-02-12 03:32:57 -0300

    feat: create footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../index.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>Copyright © 2024 Techverse. Todos os direitos reservados.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
